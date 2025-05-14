d444c3fc51170362c349d94e7f720393a236c593
Mila <dolontaeva00@gmail.com>
2025-05-14 15:35:31 -0700

    Update basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
+  basePath: '/DolontaevaanalysisWebCite',
   images: {
     unoptimized: true,
   },
