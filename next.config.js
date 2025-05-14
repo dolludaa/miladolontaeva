37849f9599f9e6aa2e73c4bf9c84136a5a46fe8d
Mila <dolontaeva00@gmail.com>
2025-05-14 15:39:16 -0700

    Update project configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/DolontaevaanalysisWebCite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/DolontaevaanalysisWebCite/' : '',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
