9d09ff8ebfdad56e17c61e2e16a0b5cac650bf54
Mila <dolontaeva00@gmail.com>
2025-05-14 15:43:03 -0700

    Update configuration for static site deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
   images: {
     unoptimized: true,
   },
+  distDir: 'dist',
 }
 
 module.exports = nextConfig 
