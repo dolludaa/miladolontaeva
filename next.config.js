6f4d69cef82ffad7fb36169dc0fb0872e7c8c806
Mila <dolontaeva00@gmail.com>
2025-05-14 15:33:13 -0700

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  basePath: '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
